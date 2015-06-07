253aecfda26bed39a472a4d62c17c908d1695f21
Graham Swan <thinkswan@gmail.com>
2015-06-07 23:04:16 +0700

    Add Flux stores

---
+++ js/stores/CartStore.js
@@ -0,0 +1,101 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var FluxCartConstants = require('../constants/FluxCartConstants');
+var EventEmitter = require('events').EventEmitter;
+var _ = require('underscore');
+
+// Define initial data points
+var _products = {};
+var _cartVisible = false;
+
+// Add product to cart
+function add(sku, update) {
+  update.quantity = (sku in _products) ? _products[sku].quantity + 1 : 1;
+  _products[sku] = _.extend({}, _products[sku], update)
+}
+
+// Set cart visibility
+function setCartVisible(cartVisible) {
+  _cartVisible = cartVisible;
+}
+
+// Remove item from cart
+function removeItem(sku) {
+  delete _products[sku];
+}
+
+// Extend Cart Store with EventEmitter to add event capabilities
+var CartStore = _.extend({}, EventEmitter.prototype, {
+
+  // Return cart items
+  getCartItems: function() {
+    return _products;
+  },
+
+  // Return number of items in cart
+  getCartCount: function() {
+    return Object.keys(_products).length;
+  },
+
+  // Return total cost of cart
+  getCartTotal: function() {
+    var total = 0;
+    for (product in _products) {
+      if (_products.hasOwnProperty(product)) {
+        total += _products[product].price * _products[product].quantity;
+      }
+    }
+
+    return total.toFixed(2);
+  },
+
+  // Return cart visibility state
+  getCartVisible: function() {
+    return _cartVisible;
+  },
+
+  // Emit change event
+  emitChange: function() {
+    this.emit('change');
+  },
+
+  // Add change listener
+  addChangeListener: function(callback) {
+    this.on('change', callback);
+  },
+
+  // Remove change listener
+  removeChangeListener: function(callback) {
+    this.removeListener('change', callback);
+  }
+
+});
+
+// Register callback with AppDispatcher
+AppDispatcher.register(function(payload) {
+  var action = payload.action;
+  var text;
+
+  switch(action.actionType) {
+    case FluxCartConstants.CART_ADD:
+      add(action.sku, action.update);
+      break;
+
+    case FluxCartConstants.CART_VISIBLE:
+      setCartVisible(action.cartVisible);
+      break;
+
+    case FluxCartConstants.CART_REMOVE:
+      removeItem(action.sku);
+      break;
+
+    default:
+      return true;
+  }
+
+  // If action was responded to, emit change event
+  CartStore.emitChange();
+
+  return true;
+});
+
+module.exports = CartStore;
---
+++ js/stores/ProductStore.js
@@ -0,0 +1,75 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var FluxCartConstants = require('../constants/FluxCartConstants');
+var EventEmitter = require('events').EventEmitter;
+var _ = require('underscore');
+
+// Define initial data points
+var _product = {};
+var _selected = null;
+
+// Load product data from mock API
+function loadProductData(data) {
+  _product = data[0];
+  _selected = data[0].variants[0];
+}
+
+// Set currently selected product variant
+function setSelected(index) {
+  _selected = _product.variants[index];
+}
+
+// Extend ProductStore with EventEmitter to add event capabilities
+var ProductStore = _.extend({}, EventEmitter.prototype, {
+
+  // Return product data
+  getProduct: function() {
+    return _product;
+  },
+
+  // Return selected product
+  getSelected: function(){
+    return _selected;
+  },
+
+  // Emit change event
+  emitChange: function() {
+    this.emit('change');
+  },
+
+  // Add change listener
+  addChangeListener: function(callback) {
+    this.on('change', callback);
+  },
+
+  // Remove change listener
+  removeChangeListener: function(callback) {
+    this.removeListener('change', callback);
+  }
+
+});
+
+// Register callback with AppDispatcher
+AppDispatcher.register(function(payload) {
+  var action = payload.action;
+  var text;
+
+  switch(action.actionType) {
+    case FluxCartConstants.RECEIVE_DATA:
+      loadProductData(action.data);
+      break;
+
+    case FluxCartConstants.SELECT_PRODUCT:
+      setSelected(action.data);
+      break;
+
+    default:
+      return true;
+  }
+
+  // If action was responded to, emit change event
+  ProductStore.emitChange();
+
+  return true;
+});
+
+module.exports = ProductStore;
